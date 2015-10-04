d88ad3f6a825a004b58935d15c812bfe176a7538
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-10-04 17:46:00 -0500

    Modified server/app.js for Heroku

--- server/app.js
+++ server/app.js
@@ -11,9 +11,10 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + "/public/assets/views/index.html");
 });
 
-var server = app.listen(3000, function() {
-   var port = server.address().port;
-    console.log('Listening on port: ', port);
+app.set("port", (process.env.PORT || 5000));
+
+var server = app.listen(app.get("port"), function(){
+    console.log("Listening on port: " + app.get("port"));
 });
 
 module.exports = app;
